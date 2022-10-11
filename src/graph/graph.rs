aa5fbe84525c91d385aa12f438596d08e42a2e4a
Philipp Haack <PhilippHaackCode@gmail.com>
2022-10-11 12:19:53 +0200

    resolve variable

--- src/graph/graph.rs
+++ src/graph/graph.rs
@@ -26,6 +26,15 @@ impl Graph {
         }
     }
 
+    pub fn add_relations(&mut self, points1: Vec<Box<Point>>, points2: Vec<Box<Point>>) {
+        if points1.len() != points2.len() {
+            panic!("The number of points in the first vector must be equal to the number of points in the second vector");
+        }
+        for i in 0..points1.len() {
+            self.add_relation(points1[i].clone(), points2[i].clone());
+        }
+    }
+
     pub fn remove_relation(&mut self, point1: Box<Point>, point2: Box<Point>) {
         if self.points_with_relations.contains_key(&point1) {
             self.points_with_relations
@@ -52,6 +61,15 @@ impl Graph {
         }
     }
 
+    pub fn is_point_connected_to_points(&self, point: Box<Point>, points: Vec<Box<Point>>) -> bool {
+        for point2 in points {
+            if !self.is_point_connected_to_point(point.clone(), point2) {
+                return false;
+            }
+        }
+        true
+    }
+
     pub fn get_points(&self) -> &HashMap<Box<Point>, HashSet<Box<Point>>> {
         &self.points_with_relations
     }
@@ -66,19 +84,140 @@ impl Graph {
        ,where b is connected to c and d, and c is connected to d and e
 
     */
-    pub fn get_possible_variable_resolutions(&self, find_pattern: Vec<Vec<Variable>>) -> Vec<HashMap<Variable, Box<Point>>> {
-        let mut possible_resolutions: Vec<HashMap<Variable, Box<Point>>> = Vec::new();
-        let mut point_not_suitable_for_variable: HashSet<(Box<Point>, Variable)> = HashSet::new();
+    pub fn get_possible_variable_resolutions(
+        &self,
+        find_pattern: Vec<Vec<Variable>>,
+    ) -> Vec<HashMap<Variable, Box<Point>>> {
         let requirements = Rule::get_find_pattern_requirements(&find_pattern);
-        let distinct_variables: Vec<Variable> = Rule::get_distinct_variables_in_pattern(find_pattern);
-        for (point, point_connections) in self.points_with_relations.iter() {
-            let mut possible_resolution: HashMap<Variable, Box<Point>>= HashMap::new();
-            while possible_resolution.len() < distinct_variables.len() {
-                let mut variable = distinct_variables[possible_resolution.len()];
-
+        let distinct_variables: Vec<Variable> =
+            Rule::get_distinct_variables_in_pattern(find_pattern);
+        // points_that_fit_level_one_requirements .: points that have the same number of connections as the variables
+        let mut points_that_fit_level_one_requirements: HashMap<Variable, Vec<Box<Point>>> =
+            HashMap::new();
+        for variable in distinct_variables {
+            let mut points_that_fit_level_one_requirements_for_variable: Vec<Box<Point>> =
+                Vec::new();
+            for (point, _) in self.points_with_relations.iter() {
+                if self
+                    .points_with_relations
+                    .get(point)
+                    .unwrap_or(&HashSet::new())
+                    .len()
+                    >= requirements.get(&variable).unwrap_or(&Vec::new()).len()
+                {
+                    points_that_fit_level_one_requirements_for_variable.push(point.clone());
+                }
             }
-            
+            points_that_fit_level_one_requirements.insert(
+                variable.clone(),
+                points_that_fit_level_one_requirements_for_variable.clone(),
+            );
         }
-        possible_resolutions
+        // for ex .: when the pattern is [a,b] [a, c] [a, d]: a is relatively small because it's only the Points that have 3 connections
+        // that means be resolving the variables in order from lowest possible points to highest possible points saves computation time
+
+        // sort the points_that_fit_level_one_requirements by the length of the Vec<Box<Point>> into the a Vec<(Variable, Vec<Box<Point>>)>
+        let mut sorted_points_that_fit_level_one_requirements: Vec<(Variable, Vec<Box<Point>>)> =
+            Vec::new();
+        for (variable, points) in points_that_fit_level_one_requirements.iter() {
+            sorted_points_that_fit_level_one_requirements.push((variable.clone(), points.clone()));
+        }
+        sorted_points_that_fit_level_one_requirements.sort_by(|a, b| a.1.len().cmp(&b.1.len()));
+        println!(
+            "sorted at the beginning: {:?}",
+            sorted_points_that_fit_level_one_requirements[0].1.len()
+        );
+
+        // at this point we have to start resolving the variables
+        // we will start with the variable that has the least number of possible points
+        // we will then try to resolve the other variables by checking if the points that are connected to the point that we are trying to resolve
+        // are also connected to the other points that we are trying to resolve
+        // if they are, then we can add the point to the possible resolutions
+        // if they are not, then we can't add the point to the possible resolutions
+
+        let mut possible_resolutions: Vec<HashMap<Variable, Box<Point>>> = Vec::new();
+        // we will use a Mapping between the variable and the point that we are trying to resolve
+        let mut current_resolution: HashMap<Variable, Box<Point>> = HashMap::new();
+        
+        self.resolve_variable(&sorted_points_that_fit_level_one_requirements, &distinct_variables, &current_resolution, &mut possible_resolutions);
+        /*
+        `distinct_variables` moved due to this implicit call to `.into_iter()`
+        graph.rs(92, 13): move occurs because `distinct_variables` has type `Vec<Variable>`, which does not implement the `Copy` trait
+        collect.rs(261, 18): this function takes ownership of the receiver `self`, which moves `distinct_variables`
+        graph.rs(97, 25): consider iterating over a slice of the `Vec<Variable>`'s content to avoid moving into the `for` loop: `&`
+        */
+
+        return Vec::new();
+    }
+
+    /* for every variable in order of the sorted_points_that_fit_level_one_requirements
+    we'll need a loop that goes like this:
+    for every point in the points_that_fit_level_one_requirements_for_variable
+        if the point is not already in the current_resolution
+            if the point is connected to all the points in the current_resolution
+                add the point to the current_resolution
+                if the current_resolution has the same number of elements as the distinct_variables
+                    add the current_resolution to the possible_resolutions
+                else
+                    call the function recursively with the next variable
+    */
+
+    fn resolve_variable(
+        &self,
+        sorted_points_that_fit_level_one_requirements: &Vec<(Variable, Vec<Box<Point>>)>,
+        distinct_variables: &Vec<Variable>,
+        current_resolution: &HashMap<Variable, Box<Point>>,
+        possible_resolutions: &mut Vec<HashMap<Variable, Box<Point>>>,
+    ) {
+        for (variable, points_that_fit_level_one_requirements_for_variable) in
+            sorted_points_that_fit_level_one_requirements
+        {
+            for point in points_that_fit_level_one_requirements_for_variable {
+                if !current_resolution.contains_key(&variable) {
+                    if self.is_point_connected_to_points(
+                        point.clone(),
+                        current_resolution
+                        .clone()
+                            .iter()
+                            .map(|(variable, point)| point.clone())
+                            .collect(),
+                    ) {
+                        let mut new_current_resolution = current_resolution.clone();
+                        new_current_resolution.insert(variable.clone(), point.clone());
+                        if new_current_resolution.len() == distinct_variables.len() {
+                            possible_resolutions.push(new_current_resolution);
+                        } else {
+                            self.resolve_variable(
+                                &sorted_points_that_fit_level_one_requirements.clone(),
+                                &distinct_variables,
+                                &new_current_resolution,
+                                possible_resolutions,
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn check_if_resulution_is_valid(
+        &self,
+        current_resolution: &HashMap<Variable, Box<Point>>,
+        requirements: &HashMap<Variable, Vec<Variable>>,
+    ) -> bool {
+        for (variable, point) in current_resolution.iter() {
+            for required_variable in requirements.get(variable).unwrap_or(&Vec::new()) {
+                if !self.is_point_connected_to_point(
+                    point.clone(),
+                    current_resolution
+                        .get(required_variable)
+                        .unwrap_or(&Box::new(Point::new()))
+                        .clone(),
+                ) {
+                    return false;
+                }
+            }
+        }
+        true
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
 mod point;
 use std::collections::HashMap;
-mod rule;
 mod graph;
+mod rule;
 use rand::Rng;
 use rule::rule::{Rule, Variable};
 
-
 fn main() {
+    // create a graph with 2000 points and 4000 random relations between the existing points
+    let mut graph = graph::graph::Graph::new();
+    for _ in 0..2000 {
+        graph.create_point();
+    }
+    let mut rng = rand::thread_rng();
 
-    println!("Hello, world!");
+    let all_points = graph.get_points();
+    let mut points1 = Vec::new();
+    let mut points2 = Vec::new();
+    for _ in 0..2000 {
+        let index1 = rng.gen_range(0..all_points.len());
+        let index2 = rng.gen_range(0..all_points.len());
+        let point1 = all_points.keys().nth(index1).unwrap().clone();
+        let point2 = all_points.keys().nth(index2).unwrap().clone();
+        points1.push(point1);
+        points2.push(point2)
+    }
+    graph.add_relations(points1, points2);
+
+    let find_pattern: Vec<Vec<Variable>> = vec![vec![Variable::a, Variable::b], vec![Variable::a, Variable::c], vec![Variable::a, Variable::d], vec![Variable::a, Variable::q]];
+    let r = graph.get_possible_variable_resolutions(find_pattern);
+
 }
-
